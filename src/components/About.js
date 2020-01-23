4ad59c5870ba145a50d16994f316a2813c8c5f3e
rjmpham <rjmpham@gmail.com>
2020-01-22 22:24:48 -0700

    Update Text and Links

--- src/components/About.js
+++ src/components/About.js
@@ -13,11 +13,11 @@ function About() {
                     <p className='textProfile'>
 
                         I have graduated from the University of Calgary with a degree in Computer Science. 
-                        Since then I have been learning React, JavaScript, and HTML to peruse a career in front end development. 
-                        My interests have largely  been algorithms, problem solving, and front end development.
+                        Since then I have been learning React, JavaScript, and HTML to pursue a career in front end development. 
+                        My interests include algorithms, problem solving, and front end design.
                         My current goal is to learn more technologies and stacks to become a full stack developer. 
                         What motivates me the most is the challenge that comes from working on interesting problems. 
-                        Feel free to take a look at my resume to get an in-depth look at my skills and projects and thank you for visiting my personal website.
+                        Feel free to take a look at my resume to get an in-depth look at my skills, or look at my projects. Thank you for visiting my personal website.
                     </p>
                 </div>
 
